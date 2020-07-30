dc87aadd2db80d6b88074f23b0827c4d720518fa
zhangyan <zhangyan@MacBook-Pro-3.local>
2020-07-30 17:16:28 +0800

    增加.gitignore

--- src/com/zyan/meituan/Main421.java
+++
@@ -1,7 +0,0 @@
-package com.zyan.meituan;
-
-public class Main421 {
-    public static void main(String[] args) {
-
-    }
-}
--- src/com/zyan/meituan/Main424.java
+++
@@ -1,9 +0,0 @@
-package com.zyan.meituan;
-
-public class Main424 {
-    public static void main(String[] args) {
-
-    }
-
-    
-}
